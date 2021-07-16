cb658a6604548ff25b99488eaf6fdc8f055c4493
이혜진 <4mikemendes@gmail.com>
2021-07-16 13:50:50 +0200

    clear

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
